f66b42054aa19a6cbf5bb888f802eeb62e8608b4
João Mateus <joao.sousa1601@aluno.ce.gov.br>
2025-10-14 14:29:09 +0000

    feito!!!

--- 4-bimestre/ex03-loop-for/index.js
+++ 4-bimestre/ex03-loop-for/index.js
@@ -1,4 +1,12 @@
 // CRIE UMA LÓGICA AQUI
+let numero1 = 0
+
+for(i = 1 ; i <= 10 ; i++){
+    numero1 = numero1 + i
+    console.log(numero1)
+}
+
+let resultado = numero1
 
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
 module.exports = resultado
