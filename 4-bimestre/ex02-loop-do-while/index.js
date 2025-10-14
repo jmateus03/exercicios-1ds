c76a1504db449815295a62f0c36647906b0410d4
João Mateus <joao.sousa1601@aluno.ce.gov.br>
2025-10-14 13:31:16 +0000

    ex2-resolvido

--- 4-bimestre/ex02-loop-do-while/index.js
+++ 4-bimestre/ex02-loop-do-while/index.js
@@ -1,4 +1,12 @@
 // CRIE UMA LÓGICA AQUI
+let numero = 5
+
+do{
+    numero += 1
+    console.log(`Contador: ${numero}`)
+}while(numero != 10)
+
+    let resultado = numero
 
 // === NÃO MODIFIQUE NADA ABAIXO ============
 module.exports = resultado
