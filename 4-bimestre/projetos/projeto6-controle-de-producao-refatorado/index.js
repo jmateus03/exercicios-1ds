5102409fecf6368ceef29c173a54e4576b0e0178
João Mateus <joao.sousa1601@aluno.ce.gov.br>
2025-10-29 12:35:23 +0000

    Feito!!!

--- 4-bimestre/projetos/projeto6-controle-de-producao-refatorado/index.js
+++ 4-bimestre/projetos/projeto6-controle-de-producao-refatorado/index.js
@@ -1,5 +1,5 @@
 // CRIE UMA LÓGICA ABAIXO
-// CRIE UMA LÓGICA ABAIXO
+
 const prompt = require("prompt-sync")()
 
 const producao = []
@@ -10,9 +10,9 @@ for(let i = 0; i < diasRegistrados; i++){
     producao.push(parseInt(prompt(`Digite a quantidade produzida no dia ${i + 1}: `)))
 }
 
-let somaArray = 0
+let total = 0
 for(i = 0; i < producao.length; i++){
-     somaArray = somaArray + producao[i]
+     total = total + producao[i]
 }
 
 let maior = producao[0]
@@ -27,12 +27,12 @@ for(i = 1; i < producao.length; i++) {
     }
 }
 
-let mediaDiaria = somaArray / producao.length
+let media = total / producao.length
 
-console.log("--- RELATÓRIO DE PRODUÇÃO ---")
+console.log("\n--- RELATÓRIO DE PRODUÇÃO ---")
 console.log(`Produções registradas: [${producao}]`)
-console.log(`Total produzido: ${somaArray}`)
-console.log(`Média diária: ${mediaDiaria}`)
+console.log(`Total produzido: ${total}`)
+console.log(`Média diária: ${media.toFixed(2)}`)
 console.log(`Maior produção: ${maior} (dia ${producao.indexOf(maior) + 1})`) 
 console.log(`Menor produção: ${menor} (dia ${producao.indexOf(menor) + 1})`)
 let crescente = producao.sort((a, b) => a - b)
@@ -40,7 +40,7 @@ console.log(`Ordem crescente: [${crescente}]`)
 let decrescente = producao.sort((a, b) => b - a)
 console.log(`Ordem decrescente: [${decrescente}]`)
 
-if(mediaDiaria >= crescente[0] / 2){
+if(media >= crescente[0] / 2){
     console.log("Produção estável!")
 }else{
     console.log("Produção abaixo do ideal.")
