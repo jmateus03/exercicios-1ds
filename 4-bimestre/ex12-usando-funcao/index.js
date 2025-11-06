5de7d0a27b2470a041da24addfb1ab5093be86d3
João Mateus <joao.sousa1601@aluno.ce.gov.br>
2025-11-06 23:07:38 +0000

    Feito!!!!

--- 4-bimestre/ex12-usando-funcao/index.js
+++ 4-bimestre/ex12-usando-funcao/index.js
@@ -1,4 +1,13 @@
 // CRIE UMA LÓGICA AQUI
 
+function multiplicar(a, b){
+    return a * b
+}
+function mostrarResultado(){
+    console.log(`Resultado da multiplicação: ${multiplicar(2, 10)}`)
+}
+
+mostrarResultado()
+
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
 module.exports = { multiplicar, mostrarResultado }
